0862bba8f3dd399dc105ddfc7ce9b54dc050767a
Student <huntergerald00@gmail.com>
2022-05-10 15:49:54 +0200

    czwart

--- app/src/main/java/com/example/grafika/OpenGLRenderer.java
+++ app/src/main/java/com/example/grafika/OpenGLRenderer.java
@@ -9,9 +9,14 @@ import javax.microedition.khronos.opengles.GL10;
 
 public class OpenGLRenderer implements GLSurfaceView.Renderer {
 
+    private Square mySquare;
+
     @Override
     public void onSurfaceCreated(GL10 gl10, javax.microedition.khronos.egl.EGLConfig eglConfig) {
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        mySquare = new Square();
+
+
     }
 
     @Override
@@ -25,6 +30,19 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
         float r = MainActivity.r;
         float g = MainActivity.g;
         GLES20.glClearColor(r, g, 0.0f, 1.0f);
+    }
+
+    public static int loadShader(int type, String shaderCode){
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
     }
 
 
